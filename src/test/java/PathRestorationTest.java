3d03487b4a9b48a670727c2e78d7c49ecb0856a6
Da Li <njulida@outlook.com>
2020-03-29 15:09:36 +0800

    test done.

--- src/test/java/PathRestorationTest.java
+++ src/test/java/PathRestorationTest.java
@@ -20,46 +20,54 @@ public class PathRestorationTest {
 
 
     static String basic_data_file_path = "src/test/resources/inputs/basic-data-20200319.xls";
-    static String test_data_file_path  = "src/test/resources/inputs/test-data-with-oracle-20200327.txt";
 
+    static String test_data_file_path_1  = "src/test/resources/inputs/test-data-with-oracle-20200327.txt";
     static String test_data_file_path_2  = "src/test/resources/inputs/test-data-with-oracle-2020032702.txt";
+    static String test_data_file_path_3  = "src/test/resources/inputs/origin-chen-17.txt";
+    static String test_data_file_path_4  = "src/test/resources/inputs/middle-chen-17.txt";
 
+    static List<String> originDPResults = new ArrayList<>();
+    static int count = 0;
 
-    @Parameterized.Parameters(name = "{index}: assertEquals(DPResult, ManualResult)")
+//    @Parameterized.Parameters(name = "{index}: assertEquals(DPResult, ManualResult)")
+    @Parameterized.Parameters(name = "{index}: assertEquals(from origin, from chen)")
     public static Collection<Object> data() throws IOException {
-        // test case 1
         Collection<Object> retList = new ArrayList<>();
-        FileInputStream fileInputStream = new FileInputStream(test_data_file_path);
+
+//        readAFile(retList, test_data_file_path_1, false);
+//        readAFile(retList, test_data_file_path_2, false);
+
+        readAFile(retList, test_data_file_path_3, true);
+        readAFile(retList, test_data_file_path_4, true);
+
+        return retList;
+    }
+
+    private static void readAFile(Collection<Object> retList, String test_data_file, boolean broken) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(test_data_file);
         BufferedReader br = new BufferedReader(new InputStreamReader(fileInputStream));
 
-        String strLine; int count = 0;
+        String strLine;
         while ((strLine = br.readLine()) != null) {
-//            System.out.print( count + ": ");
-            count++;
             JSONObject jsonObject = new JSONObject(strLine);
             jsonObject.put("basicDataPath", basic_data_file_path);
             jsonObject.put("index", count);
+
+            //make up with missing attributes in xu and chen's data.
+            if (broken) {
+                jsonObject.put("enTime", "");
+                jsonObject.put("exTime", "");
+                jsonObject.put("modifyCost", 0.01);
+                jsonObject.put("addCost", 0.1);
+                jsonObject.put("deleteCost", 500);
+                jsonObject.put("deleteCost2", 2);
+                jsonObject.put("deleteEndCost", 100000);
+            }
+
             retList.add(jsonObject);
-//            if (count > 10) break;
-        }
-        fileInputStream.close();
-
-        // test case 2
-        fileInputStream = new FileInputStream(test_data_file_path_2);
-        br = new BufferedReader(new InputStreamReader(fileInputStream));
-
-        count = 0;
-        while ((strLine = br.readLine()) != null) {
-//            System.out.print( count + ": ");
             count++;
-            JSONObject jsonObject = new JSONObject(strLine);
-            jsonObject.put("basicDataPath", basic_data_file_path);
-            jsonObject.put("index", count);
-            retList.add(jsonObject);
-//            if (count > 10) break;
         }
         fileInputStream.close();
-        return retList;
     }
 
     @Parameterized.Parameter
@@ -67,19 +75,37 @@ public class PathRestorationTest {
 
     @Test
     public void testPathRestorationWithNewCases()  {
+        PathRestoration pathRestoration = new PathRestoration();
+        pathRestoration.pathRestorationMethod(testCase.toString());
+        String manualResult = testCase.getString("manualResult");
+        String DPResult = pathRestoration.recoveredPath.getLiteralPath();
+
+        assertEquals(manualResult, DPResult);
+    }
+
+    @Test
+    public void testPathRestorationByChen()  {
         System.out.println("index = " + testCase.getInt("index"));
         PathRestoration pathRestoration = new PathRestoration();
-        String returnedJSONString = pathRestoration.pathRestorationMethod(testCase.toString());
-        JSONObject jsonObject = new JSONObject(returnedJSONString);
+        pathRestoration.pathRestorationMethod(testCase.toString());
         String manualResult = testCase.getString("manualResult");
         String DPResult = pathRestoration.recoveredPath.getLiteralPath();
 //        System.out.println(DPResult);
 //        System.out.println(manualResult);
 
-        assertEquals(manualResult, DPResult);
+        int cur = testCase.getInt("index");
+        System.out.println("cur = " + cur);
+        if (cur < 17) {
+            System.out.println("cur = " + cur + " DPResult = " + DPResult);
+            originDPResults.add(DPResult);
+        }
+        else {
+            System.out.println("i = " + cur%17 + " j = " + cur);
+            assertEquals(originDPResults.get(cur%17), DPResult);
+        }
+//        assertEquals(manualResult, DPResult);
+
     }
-
-
 
     JSONObject successJsonObject = new JSONObject();
     JSONObject failureJsonObject = new JSONObject();
